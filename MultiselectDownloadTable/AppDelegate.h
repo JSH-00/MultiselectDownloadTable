451791f8b97b9822f406eebc5a5ee34599e14612
JSH-00 <jshjshh@163.com>
2020-10-05 18:33:51 +0800

    Initial Commit

---
+++ MultiselectDownloadTable/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MultiselectDownloadTable
+//
+//  Created by JSH on 2020/10/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MultiselectDownloadTable/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MultiselectDownloadTable
+//
+//  Created by JSH on 2020/10/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
